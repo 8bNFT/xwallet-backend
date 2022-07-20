952a1c30fdf0f668edcf767e66537dd42bb260c5
8bNFT <92310321+8bNFT@users.noreply.github.com>
2022-07-20 19:15:53 +0200

    concurrency is hard

--- coin.mjs
+++ coin.mjs
@@ -4,22 +4,43 @@ import fs from 'fs'
 class Coins{
     constructor(){
         this.coins = {}
+        this.interval = {}
     }
 
-    async readCoins(network = 'mainnet'){
-        if(this.coins[network] && Date.now() - this.coins[network].expiration < 900000) return this.coins[network].data
+    readCoins(network = 'mainnet'){
+        if(this.coins[network]) return this.coins[network].data
+        return {}
+    }
+
+    lastUpdate(network = 'mainnet'){
+        if(this.coins[network]) return this.coins[network].updated_at
+        return false
+    }
+
+    async initialize(){
+        const networks = ["mainnet", "testnet"]
+        for(let network of networks){
+            await this.tryStaticInit(network)
+            this.interval[network] = setInterval(() => this.fetchTokenInformation(network), 900000)
+            console.log(`Loaded token data and initialized interval for`, network)
+        }
+
+        console.log('Initialized:', networks.join(", "))
+    }
+
+    async tryStaticInit(network){
         try{
-            const _coins = JSON.parse(fs.readFileSync(`coins_${network}.json`).toString())
-            if(Date.now() - _coins.expiration < 900000){
-                this.coins[network] = _coins.data
-                return this.coins[network]
-            }
-        }catch{}
-        this.coins[network] = await this.fetchPrices(network)
-        return this.coins[network].data
+            const { data, updated_at } = JSON.parse(fs.readFileSync(`coins_${network}.json`))
+            if(Date.now() - updated_at < 900000) return { data, updated_at }
+
+            return this.fetchTokenInformation(network)
+        }catch(e){
+            console.log(e)
+            return this.fetchTokenInformation(network)
+        }
     }
 
-    async fetchPrices(network){
+    async fetchTokenInformation(network){
         const { result } = await (await fetch(`https://api.${network == 'testnet' && 'ropsten.' || ''}x.immutable.com/v1/tokens`)).json()
         const coins = {}
         let symbols = []
@@ -38,30 +59,30 @@ class Coins{
             symbols.push(coin.symbol)
         }
 
-        const { data } = await (await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=27b33efc-d2c2-4a18-8056-67ecad6e2720&symbol=${symbols.join(',')}&skip_invalid=true`)).json()
-        for(let [symbol, _stats] of Object.entries(data)){
-            const token_address = symbol === "ETH" ? "ETH" : (_stats.platform?.token_address || "").toLowerCase()
-            if(!token_address || !coins[token_address]) continue
-
-            const stats = _stats.quote.USD
-            coins[token_address] = {
-                name: _stats.name,
-                symbol,
-                ...coins[token_address],
-                price: stats.price,
-                change: {
-                    '1h': parseFloat((stats.percent_change_1h || 0).toFixed(2)),
-                    '1d': parseFloat((stats.percent_change_24h || 0).toFixed(2)),
-                    '7d': parseFloat((stats.percent_change_7d || 0).toFixed(2)),
-                    '30d': parseFloat((stats.percent_change_30d || 0).toFixed(2))
+        if(network === "mainnet"){
+            const { data } = await (await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=27b33efc-d2c2-4a18-8056-67ecad6e2720&symbol=${symbols.join(',')}&skip_invalid=true`)).json()
+            for(let [symbol, _stats] of Object.entries(data)){
+                const token_address = symbol === "ETH" ? "ETH" : (_stats.platform?.token_address || "").toLowerCase()
+                if(!token_address || !coins[token_address]) continue
+    
+                const stats = _stats.quote.USD
+                coins[token_address] = {
+                    name: _stats.name,
+                    symbol,
+                    ...coins[token_address],
+                    price: stats.price,
+                    change: {
+                        '1h': parseFloat((stats.percent_change_1h || 0).toFixed(2)),
+                        '1d': parseFloat((stats.percent_change_24h || 0).toFixed(2)),
+                        '7d': parseFloat((stats.percent_change_7d || 0).toFixed(2)),
+                        '30d': parseFloat((stats.percent_change_30d || 0).toFixed(2))
+                    }
                 }
             }
         }
 
-        const tokenInfo = {expiration: Date.now(), data: coins}
-
-        fs.writeFileSync(`coins_${network}.json`, JSON.stringify(tokenInfo))
-        return tokenInfo
+        this.coins[network] = {data: coins, updated_at: Date.now()}
+        fs.writeFileSync(`coins_${network}.json`, JSON.stringify({data: coins, updated_at: Date.now()}))
     }
 }
 
--- index.mjs
+++ index.mjs
@@ -7,17 +7,27 @@ const fastify = _f()
 fastify.register(cors, {})
 
 fastify.get('/tokens', async (req, res)=>{
-    return await coins.readCoins()
+    return coins.readCoins()
 })
 
-fastify.get('/tokens/mainnet', async (req, res)=>{
-    return await coins.readCoins()
+fastify.get('/tokens/:network', async (req, res)=>{
+    return coins.readCoins(req.params.network.toLowerCase())
 })
 
-fastify.get('/tokens/testnet', async (req, res)=>{
-    return await coins.readCoins('testnet')
+fastify.get('/latest-update/:network', async(req, res)=>{
+    const updated_at = coins.lastUpdate(req.params.network.toLowerCase())
+    if(!updated_at) return { updated_at: false }
+
+    return { updated_at: new Date(updated_at).toISOString() }
 })
 
-fastify.listen(3000)
-.then(console.log)
-.catch(console.error)
+;(async()=>{
+    await coins.initialize()
+    try{
+        const host = await fastify.listen(3333)
+        console.log("Running token server on", host)
+    }catch(err){
+        console.error(err)
+        process.exit(1)
+    }
+})()
